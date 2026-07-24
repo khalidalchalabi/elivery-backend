6a8dd50e46c5faf197e3daaa28e2b988088af4ab
fox <fox@fox.fox>
2026-07-24 15:38:36 +0300

    Update customer web build with instant launch and categories optimizations

--- public/customer/flutter_bootstrap.js
+++ public/customer/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"3452d735bd38224ef2db85ca763d862d6326b1
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2493047808" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "529241529" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
