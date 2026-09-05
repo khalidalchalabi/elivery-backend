af4ca98af4d14c7bc994574edd2c38b19db1ba84
khalidalchalabi <155018988+khalidalchalabi@users.noreply.github.com>
2026-09-05 10:29:34 +0300

    public/web: تنسيق الأرقام المالية بفواصل الآلاف

--- public/web/flutter_bootstrap.js
+++ public/web/flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"a804b261645ef8c13eb3d5c44a5c2fb0340c55
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3924482534" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1530286927" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
